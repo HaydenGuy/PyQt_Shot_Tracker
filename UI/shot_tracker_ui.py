3293d03ebbfb65dfb7bfd312f755751039377010
Linux Desktop <haydenguyn@gmail.com>
2023-07-23 22:03:33 -0700

    Added logic to update the spreadsheet when the user updates the GUI Spreadsheet. Added logic to set the background colors based on the cell data

--- UI/shot_tracker_ui.py
+++ UI/shot_tracker_ui.py
@@ -23,20 +23,26 @@ class Ui_shot_tracker(object):
     def setupUi(self, shot_tracker):
         if not shot_tracker.objectName():
             shot_tracker.setObjectName(u"shot_tracker")
-        shot_tracker.resize(810, 546)
+        shot_tracker.resize(1099, 380)
         self.centralwidget = QWidget(shot_tracker)
         self.centralwidget.setObjectName(u"centralwidget")
+        font = QFont()
+        font.setPointSize(13)
+        self.centralwidget.setFont(font)
         self.verticalLayout = QVBoxLayout(self.centralwidget)
         self.verticalLayout.setObjectName(u"verticalLayout")
         self.tree_view = QTreeView(self.centralwidget)
         self.tree_view.setObjectName(u"tree_view")
+        font1 = QFont()
+        font1.setPointSize(12)
+        self.tree_view.setFont(font1)
 
         self.verticalLayout.addWidget(self.tree_view)
 
         shot_tracker.setCentralWidget(self.centralwidget)
         self.menubar = QMenuBar(shot_tracker)
         self.menubar.setObjectName(u"menubar")
-        self.menubar.setGeometry(QRect(0, 0, 810, 23))
+        self.menubar.setGeometry(QRect(0, 0, 1099, 20))
         shot_tracker.setMenuBar(self.menubar)
         self.statusbar = QStatusBar(shot_tracker)
         self.statusbar.setObjectName(u"statusbar")
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys, os.path
 from PySide6.QtWidgets import QMainWindow, QApplication
 from PySide6.QtGui import QStandardItemModel, QStandardItem
+from PySide6.QtCore import Qt
 
 from UI.shot_tracker_ui import Ui_shot_tracker
 
@@ -25,25 +26,24 @@ class ShotTracker(QMainWindow, Ui_shot_tracker):
         
         # creates a model item which will display the text we pull from the spreadsheet
         self.item_model = QStandardItemModel()
-        
 
         # Taken from the Google quickstart python guide
         creds = None
         # The file token.json stores the user's access and refresh tokens, and is
         # created automatically when the authorization flow completes for the first
         # time.
-        if os.path.exists('Shot_Tracker/token.json'):
-            creds = Credentials.from_authorized_user_file('Shot_Tracker/token.json', SCOPES)
+        if os.path.exists('Qt/Qt_Shot_Tracker/token.json'):
+            creds = Credentials.from_authorized_user_file('Qt/Qt_Shot_Tracker/token.json', SCOPES)
         # If there are no (valid) credentials available, let the user log in.
         if not creds or not creds.valid:
             if creds and creds.expired and creds.refresh_token:
                 creds.refresh(Request())
             else:
                 flow = InstalledAppFlow.from_client_secrets_file(
-                    'Shot_Tracker/credentials.json', SCOPES)
+                    'Qt/Qt_Shot_Tracker/credentials.json', SCOPES)
                 creds = flow.run_local_server(port=0)
             # Save the credentials for the next run
-            with open('Shot_Tracker/token.json', 'w') as token:
+            with open('Qt/Qt_Shot_Tracker/token.json', 'w') as token:
                 token.write(creds.to_json())
 
         try:
@@ -51,15 +51,15 @@ class ShotTracker(QMainWindow, Ui_shot_tracker):
             service = build('sheets', 'v4', credentials=creds)
 
             # Call the Sheets API
-            sheet = service.spreadsheets()
+            self.sheet = service.spreadsheets()
 
             '''
                 Initialise a column titles variable which gets the values of the spreadsheet from range A1:G1
                 Assigns those values to a list called column title values
                 Sets the number of columns in item model to be equal to the length of the list of items
             '''
-            column_titles = sheet.values().get(spreadsheetId=SPREADSHEET_ID,
-                                            range='Sheet1!A1:G1').execute()
+            column_titles = self.sheet.values().get(spreadsheetId=SPREADSHEET_ID,
+                                            range='Sheet1!A1:H1').execute()
             column_title_values = column_titles.get('values', [])
             self.item_model.setColumnCount(len(column_title_values[0]))
 
@@ -80,9 +80,9 @@ class ShotTracker(QMainWindow, Ui_shot_tracker):
                 Initialise a row info variable which gets the values of the spreadsheet from range A2:G11
                 Assigns those values to a list called row info values
             '''
-            row_info = sheet.values().get(spreadsheetId=SPREADSHEET_ID,
-                                            range='Sheet1!A2:G11').execute()
-            row_info_values = row_info.get('values', [])
+            row_info = self.sheet.values().get(spreadsheetId=SPREADSHEET_ID,
+                                            range='Sheet1!A2:H11').execute()
+            self.row_info_values = row_info.get('values', [])
 
             '''
                 This pulls the data from the spreadsheet and populates the item model
@@ -92,7 +92,7 @@ class ShotTracker(QMainWindow, Ui_shot_tracker):
                 The values are assigned to a QStandardItem
                 The item model at row(i), column(i), is set to the item(i)
             '''
-            for row, info in enumerate(row_info_values):
+            for row, info in enumerate(self.row_info_values):
                 for column, title in enumerate(info):
                     item = QStandardItem(title)
                     self.item_model.setItem(row, column, item)
@@ -100,10 +100,77 @@ class ShotTracker(QMainWindow, Ui_shot_tracker):
             # the tree view is set to display the model item model
             self.tree_view.setModel(self.item_model)
 
+            # resizes the columns so they are maximised to the size of their text
+            for column in range(self.item_model.columnCount()):
+                self.tree_view.resizeColumnToContents(column)
+
+            self.apply_background_colors()
+
         except HttpError as err:
             print(err)
 
+        # if the user updates the data in the GUI we call the update_data method
+        self.item_model.dataChanged.connect(self.update_data)
 
+    def update_data(self, topLeft, bottomRight):
+        # Returns the data that the user entered by reading the topLeft to bottomRight of the item_model when it's data changes
+        for row in range(topLeft.row(), bottomRight.row() + 1):
+            for column in range(topLeft.column(), bottomRight.column() + 1):
+                index = self.item_model.index(row, column)
+                item = self.item_model.itemFromIndex(index)
+                user_data = item.data(Qt.DisplayRole)
+        
+        '''
+            Gets the cell number for the user changes
+            The column letter is obtained by converting the column index to its ASCII character code (A = 65)
+            The row number is incremented by 2 to match the 1-based index used in the spreadsheet (starts at 1)
+        '''
+        cell_range = f'Sheet1!{chr(65 + column)}{row + 2}'
+        # the value of the spreadsheet is updated to match whatever the user input on the GUI shot tracker
+        self.sheet.values().update(spreadsheetId=SPREADSHEET_ID, range=cell_range,
+                                    valueInputOption='USER_ENTERED', body={'values': [[f'{user_data}']]}).execute()
+        
+        self.apply_background_colors()
+    
+    def apply_background_colors(self):
+        # Iterate through all rows in the model
+        for row in range(self.item_model.rowCount()):
+            # Initialize boolean flags for each row
+            is_finished = False
+            is_in_progress = False
+            is_not_finished = False
+
+            for column in range(self.item_model.columnCount()):
+                index = self.item_model.index(row, column)
+                item = self.item_model.itemFromIndex(index)
+
+                # Check if the item's data contains the desired content (e.g., "Finished")
+                if item.text() == 'Finished':
+                    is_finished = True
+                elif item.text() == 'In Progress':
+                    is_in_progress = True
+                elif item.text() == 'Not Finished':
+                    is_not_finished = True
+                    
+            # Update the entire row (row index 'row') with the desired background color
+            if is_finished:
+                for column in range(self.item_model.columnCount()):
+                    index = self.item_model.index(row, column)
+                    item = self.item_model.itemFromIndex(index)
+                    item.setBackground(Qt.green)  # Set the desired background color
+            elif is_in_progress:
+                for column in range(self.item_model.columnCount()):
+                    index = self.item_model.index(row, column)
+                    item = self.item_model.itemFromIndex(index)
+                    item.setBackground(Qt.yellow)  # Set the desired background color
+            elif is_not_finished:
+                for column in range(self.item_model.columnCount()):
+                    index = self.item_model.index(row, column)
+                    item = self.item_model.itemFromIndex(index)
+                    item.setBackground(Qt.white)  # Set the desired background color
+            
+                
+        
 if __name__ == '__main__':
     app = QApplication(sys.argv)
 
